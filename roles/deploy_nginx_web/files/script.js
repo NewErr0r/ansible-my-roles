b9faef6b1005fcaf5f4ab09ae0fcf172d80faa40
”Dima <sechmma@gmail.com>
2022-06-21 21:06:45 +0300

    first commit

---
+++ roles/deploy_nginx_web/files/script.js
@@ -0,0 +1,12 @@
+
+function marian()
+{   document.getElementById('pokaz').innerHTML='';
+    let pokaz=document.getElementById('pokaz');
+   
+    pokaz.insertAdjacentHTML('beforeend', "<img style=align:center src=marian.webp>");
+}
+function puerto()
+{   document.getElementById('pokaz').innerHTML='';
+    let pokaz=document.getElementById('pokaz');
+    pokaz.insertAdjacentHTML('beforeend', "<img src='puerto.jpg'>");
+}
